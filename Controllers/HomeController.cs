bd6c24247a643a19d7d9d3da7c8a8a44e5497f23
LamkhadeAishwarya <ashwinilamkhade2020@gmail.com>
2023-12-06 16:57:47 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using Jisa.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jisa.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public UserDAL da=new UserDAL();    
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+       public IActionResult Login(User us)
+        {
+            int result = da.Validation(us);
+            if (result > 0)
+            {
+                ViewBag.Message = " Login Successfully";
+            }
+            else
+            {
+                ViewBag.Message = " Login Fail";
+            }
+            return View();
+           
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/UserDAL.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jisa.Models
+{
+    public class UserDAL
+    {
+        List<User> Users;
+        public UserDAL()
+        {
+            Users = new List<User>()
+            {
+                new User {UserName="Aishwarya",Email="aishwaryalamkhade@gmail.com",Password="123" }
+            };
+            
+           
+    
+             
+        }
+       public int Validation(User u)
+        {
+            int res = 0;
+            foreach (var user in Users)
+            {
+                if (u.UserName == user.UserName)
+                {
+                    res = 1;
+                }
+            }
+            return res;
+        }
+
+
+    }
+}
